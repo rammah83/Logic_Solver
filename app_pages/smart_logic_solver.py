8adaccad10aaf2b9336865c8f8e06c48ded38001
rammah83 <rahimi.m.hicham@gmail.com>
2024-11-22 02:19:56 +0100

    Feat: impliment automatic symbols and statements evaluation

--- app_pages/smart_logic_solver.py
+++ app_pages/smart_logic_solver.py
@@ -1,3 +1,5 @@
+from math import log
+from narwhals import col
 import sympy as sp
 from sympy.logic.boolalg import simplify_logic, to_cnf
 from sympy.logic.inference import satisfiable, valid, PropKB, entails
@@ -12,82 +14,25 @@ col_kb, col_entails, _ = st.columns(
     )
 logical_statement = col_kb.text_area(
         "*Building Knwoledge Base (KB) _Logical Statement_*",
-        help="use '>>' for 'implies', '&' for 'and', '|' for 'or', '~' for 'not'",
+        help="use '>>' for 'implies', '&' for 'and', '|' for 'or', '~' for 'not': don't use E, I, O, N, S, Q",
     )
-
-selected_symbols = set(tuple(sp.sympify(statement)).atoms() for statement in logical_statement.split("\n"))
-st.write(selected_symbols)
-symbol_to_entail = col_entails.selectbox(label="Entails", options=selected_symbols)
+logical_statement = logical_statement.replace("=>", ">>").replace("<=", "<<")
+st.write(logical_statement)
 if st.button("SOLVE"):
     container = st.container()
     expender = st.expander("## Details")
     col_sentence, col_result, col_validity = expender.columns(
-        [2, 1, 1], gap="small", vertical_alignment="top"
-    )
+            [2, 1, 3], gap="small", vertical_alignment="top"
+        )
     col_sentence.write("_Statement:_")
     col_result.write("_Statement Evaluation:_")
-    col_validity.write("_Resolution:_")
-    kb: PropKB = PropKB()
     for statement in logical_statement.split("\n"):
         # display and List KB
         symp_statement = sp.sympify(statement)
         col_sentence.latex(sp.latex(symp_statement))
-        symp_result = symp_statement.subs(
-            {f"{k}": validites[f"{k}"] for k in symp_statement.atoms()}
-        )
-        col_result.latex(symp_result)
-        if valid(symp_statement):
-            col_validity.success("Valid in all Worlds")
-        else:
-            models = satisfiable(symp_statement, all_models=True)
-            if all(models):
-                with col_validity.popover(
-                    "Valid in some Worlds", use_container_width=True
-                ):
-                    for model in satisfiable(symp_statement, all_models=True):
-                        st.latex(model if model else "Impossible")
-                        # st.latex(satisfiable(symp_statement, all_models=False))
-            else:
-                col_validity.error("Invalid in all Worlds")
-        kb.tell(symp_statement)
-    # buil and stantment between logical_statement
-    global_statement = sp.sympify(
-        " & ".join(
-            [
-                "(" + str(symp_statement) + ")"
-                for symp_statement in logical_statement.split("\n")
-            ]
-        )
-    )
+        col_result.latex(sp.latex(sp.simplify_logic(symp_statement)))
     col_sentence.write("---")
-    col_sentence.latex(sp.latex(global_statement))
     col_result.write("---")
-    global_symp_result = global_statement.subs(
-        {f"{k}": validites[f"{k}"] for k in global_statement.atoms()}
-    )
-    col_result.latex(global_symp_result)
-    col_validity.write("---")
-    if valid(global_statement):
-        col_validity.success("Valid in all Worlds")
-    else:
-        models_for_global = satisfiable(global_statement, all_models=True)
-        if all(models_for_global):
-            with col_validity.popover(
-                "Valid in some possibilities", use_container_width=True
-            ):
-                for model in satisfiable(global_statement, all_models=True):
-                    st.latex(model if model else "Impossible")
-                    # st.latex(satisfiable(symp_statement, all_models=False))
-        else:
-            col_validity.error("Invalid in all Worlds")
-    st.write("---")
-    col_kb, col_entails, col_result = container.columns(
-        3, gap="small", vertical_alignment="center"
-    )
-    col_entails.write(
-        f"### Knowledge Base being TRUE entails {symbol_to_entail} is "
-    )
-    col_kb.dataframe({"Knowledge Base": kb.clauses}, hide_index=True)
-    col_result.latex(
-        kb.ask(symbol_to_entail)
-    )  # alternative : col_result.latex(entails(symbol_to_entail, kb.clauses))
+    col_sentence.write(".`.")
+
+
---
+++ utils/logical_syllogism.py
@@ -0,0 +1,119 @@
+import sympy as sp
+
+from sympy.logic.boolalg import And,  Not
+from sympy.logic import inference
+
+class PropositionalLogicSyllogism:
+    """Docstring for PropositionalLogicSyllogism
+    
+    :var global_statement: Description
+    :vartype global_statement: None 
+    :var simplified_statement: Description
+    :vartype simplified_statement: None """
+    
+    
+    global_statement = None
+    simplified_statement = None
+
+    def __init__(self, propositions):
+        # check if proposition symbols are sympy symbols compatible
+        if not all([isinstance(prop, sp.Symbol) for prop in propositions]):
+            self.propositions = propositions
+            self.global_statement = And(*sp.sympify(self.propositions))
+        else:
+            raise ValueError("Propositions must be sympy symbols")
+        self.simplified_statement = sp.simplify_logic(
+            self.global_statement, force=False
+        )
+        # display(self.global_statement, "<=>", self.simplified_statement)
+        # print("---"*50)
+        # display(list(inference.satisfiable(self.simplified_statement, all_models=True)))
+
+    def representation(self, simplify=False):
+        if simplify:
+            return sp.simplify_logic(self.global_statement, force=False)
+        else:
+            return self.global_statement
+
+    @property
+    def symbols(self):
+        return self.global_statement.atoms() | self.global_statement.free_symbols
+
+    def solve(self, negate=False):
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        
+        statement = (
+            Not(self.simplified_statement)
+            if negate
+            else self.simplified_statement.copy()
+        )
+        # check if all possible assignments are valid
+        if inference.valid(statement):
+            return "Tautology: Valid in all Possible Worlds"
+        else:
+            models = list(
+                inference.satisfiable(statement, algorithm=None, all_models=True)
+            )
+            if not any(models):
+                return "Invalid in all Possible Worlds: contraduition"
+            else:
+                n_worlds = f"{len(models)} world{'s' if len(models) > 1 else ''}"
+                # print(f"Valid in {n_worlds}: satifiable")
+                return [model for model in models]
+
+        
+
+    @staticmethod
+    def check_entailment(premises, conclusion) -> bool | str:
+        """_summary_
+
+        Args:
+            premises (_type_): _description_
+            conclusion (_type_): _description_
+
+        Returns:
+            bool | str: _description_
+        """
+        # print(sp.sympify(premises, evaluate=True))
+        know_base: inference.PropKB = inference.PropKB()
+        [know_base.tell(premise) for premise in sp.sympify(premises)]
+        print(f"{premises} ⊨ {conclusion} is:")
+        if not sp.simplify_logic(And(*sp.sympify(premises))):
+            return "Invalid Premises: Contradiction"
+        return know_base.ask(conclusion)
+
+def print_satisfaible_worlds(valid_worlds:dict|str) -> None:
+    if isinstance(valid_worlds, str):
+        print(valid_worlds)
+        return
+    print("-------logic is satisfiable in some possible worlds--------")
+    for k in sorted(valid_worlds[0].keys(), key=lambda x: str(x)):
+        print(f"{str(k):^10}", end="")
+    print("\n",f"{'-'*9*len(valid_worlds[0])}")
+    for world in valid_worlds:
+        world = dict(sorted(world.items(), key=lambda x: str(x)))
+        for value in world.values():
+            print(f"{value:^10}", end="")
+        print()
+
+
+
+if __name__ == "__main__":
+    propositions = [
+    "G & ~Ev",
+    "~Ev",
+    "~G >> R",
+    "~C & D >> G",
+    "G >> ~R",
+]
+
+    problem = PropositionalLogicSyllogism(propositions)
+    sp.pprint(problem.representation(simplify=True))
+    valid_worlds: list[dict] = problem.solve(negate=False)
+    sp.pprint(valid_worlds)
+    print_satisfaible_worlds(valid_worlds)
